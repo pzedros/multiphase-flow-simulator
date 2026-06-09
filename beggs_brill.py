a05173806ac7e17584a704120a917c2a4d46435f
pedro <pedrosamuelsjb@gmail.com>
2026-06-09 17:29:48 -0300

    Fix syntax error na docstring de gradiente_total

--- beggs_brill.py
+++ beggs_brill.py
@@ -254,6 +254,6 @@ def gradiente_total(dPdL_F, dPdL_G, EK):
     dP/dL|_T = (-(dP/dL|_F) - (dP/dL|_G)) / (1 - EK)
 
     O termo de aceleração é feito via EK, eliminando necessidade
-    de calcular dP/dL|_A soziho """"
+    de calcular dP/dL|_A sozinho."""
     dPdL_T = (-(dPdL_F) - (dPdL_G)) / (1 - EK)
     return dPdL_T
